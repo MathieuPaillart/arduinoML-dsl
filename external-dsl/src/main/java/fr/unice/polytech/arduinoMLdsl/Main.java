bc5ab83558ad445f6bee44c79dcab07a78a7f082
Mathieu PAILLART <mathieu.paillart@planisware.com>
2020-01-16 16:30:13 +0100

    all the resources files are taken into the runner. They are all written in target/classes/result with the right tag (example result/scenario1 will contain the result of the scenario1 written in arduinoml)

--- external-dsl/src/main/java/fr/unice/polytech/arduinoMLdsl/Main.java
+++ external-dsl/src/main/java/fr/unice/polytech/arduinoMLdsl/Main.java
@@ -13,24 +13,32 @@ import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
+
     public static void main(String[] args) throws Exception {
         System.out.println("\n\nRunning the ANTLR compiler for ArduinoML");
-
-        CharStream stream = getCharStream(args);
-        App theApp = buildModel(stream);
-        exportToCode(theApp);
+        File file = new File(Main.class.getClassLoader().getResource("scenario").getFile());
+        List<Path> listOfFiles = Arrays.stream(file.listFiles()).map(File::toPath).collect(Collectors.toList());
+        for (int i = 0; i < listOfFiles.size(); i++) {
+            CharStream stream = getCharStream(listOfFiles.get(i));
+            App theApp = buildModel(stream);
+            //scenario begins at 1 and not 0
+            exportToCode(theApp, i + 1);
+        }
 
     }
 
-    private static CharStream getCharStream(String[] args) throws IOException {
-        if (args.length < 1)
-            throw new RuntimeException("no input file");
-        Path input = Paths.get(new File(args[0]).toURI());
+    private static CharStream getCharStream(Path input) throws IOException {
         System.out.println("Using input file: " + input);
         return CharStreams.fromPath(input);
     }
@@ -50,10 +58,12 @@ public class Main {
         return builder.retrieve();
     }
 
-    private static void exportToCode(App theApp) {
+    private static void exportToCode(App theApp, int fileNumber) throws IOException {
         Visitor codeGenerator = new ToWiring();
         theApp.accept(codeGenerator);
-        System.out.println(codeGenerator.getResult());
+        Object content = codeGenerator.getResult();
+        File file = new File(Main.class.getClassLoader().getResource("result").getFile());
+        Files.write(Paths.get(file.toPath().toString() + "/scenario" + fileNumber + ".txt"), content.toString().getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE);
     }
 
 }
